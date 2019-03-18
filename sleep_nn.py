7d507cfab628a9340fce5484d27e1350040d97e9
Joao Palotti <joaopalotti@gmail.com>
2019-03-18 19:09:48 +0300

    Changed CNN. ToDo: use conditional variable and update LSTM model

--- sleep_nn.py
+++ sleep_nn.py
@@ -1,31 +1,46 @@
 import numpy as np
+import pandas as pd
 import sys
 import time
+import os
+import random
 #
 from sleep_misc import load_dataset
 from sleep_eval import eval_f1, eval_acc
 from sklearn.preprocessing import StandardScaler
 #
-from keras.layers import Flatten, Convolution1D
+from keras.layers import Flatten, Conv1D, concatenate, Input
 from keras.layers.core import Activation
 from keras.layers.core import Dropout
+from keras.models import Model
 from keras.models import Sequential
 from keras.models import load_model
 from keras import layers
 from keras.layers import Dense
 
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 TASK = int(sys.argv[1])
 SEQ_LEN = int(sys.argv[2]) # used in the experiments 20, 50 and 100
 NN_TYPE = sys.argv[3] # LSTM or CNN
-TRAINING = False
+TRAINING = True
+USING_MESA_VARIABLES = True
+
 np.random.seed(42)
+tf.set_random_seed(42)
+os.environ['PYTHONHASHSEED']=str(42)
+random.seed(42)
 
 # Parameters used in the experiments
 input_type = "raw"
 epochs = 10
 batch_size = 32
+
+from keras import backend as K
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
 
 MODEL_OUTFILE = "model_%s_task%d_%s_seq%d.pkl" % (NN_TYPE, TASK, input_type, SEQ_LEN)
 RESULTS_OUTFILE = "task%d_%s_%s_%d.csv" % (TASK, NN_TYPE, input_type, SEQ_LEN)
@@ -36,6 +51,15 @@ print("...Loading dataset into memory...")
 dftrain, dftest, featnames = load_dataset(DATASET_PATH, useCache=True)
 print("...Done...")
 
+
+if USING_MESA_VARIABLES:
+    mesa_cols = ["gender1", "sleepage5c", "insmnia5", "rstlesslgs5", "slpapnea5"]
+    variables = pd.read_csv("./data/mesa-sleep-dataset-0.3.0.csv.gz")[["mesaid"] + mesa_cols].fillna(0.0)
+
+    dftrain = pd.merge(dftrain, variables)
+    dftest = pd.merge(dftest, variables)
+
+
 scaler = StandardScaler()
 scaler.fit(dftrain[["activity"]].fillna(0.0))
 
@@ -43,6 +67,8 @@ dftrain["activity"] = scaler.transform(dftrain[["activity"]].fillna(0.0))
 dftest["activity"] = scaler.transform(dftest[["activity"]].fillna(0.0))
 
 def extract_x_y(df, seq_len, mesaid):
+    variables = df[df["mesaid"] == mesaid][mesa_cols].copy()
+    variables = variables.fillna(0.0).values
     df = df[df["mesaid"] == mesaid][["activity","gt"]].copy()
 
     for s in range(1,seq_len/2 + 1):
@@ -55,21 +81,23 @@ def extract_x_y(df, seq_len, mesaid):
     y = np.array([[1] if v else [0] for v in y])
     del df["gt"]
     x = df.fillna(-1).values
-    return x,y
+
+    return x, y, variables
 
 def get_data(df, seq_len):
     mesaids = df.mesaid.unique()
-    x_, y_ = extract_x_y(df, seq_len, mesaids[0])
-
+    x_, y_, variables_ = extract_x_y(df, seq_len, mesaids[0])
     for mid in mesaids[1:]:
-        x_tmp, y_tmp = extract_x_y(df, seq_len, mid)
+        x_tmp, y_tmp, variables_tmp = extract_x_y(df, seq_len, mid)
         x_ = np.concatenate((x_, x_tmp))
         y_ = np.concatenate((y_, y_tmp))
-    return x_, y_
+        variables_ = np.concatenate((variables_, variables_tmp))
 
-def build_model(input_dim, nn_type):
+    return x_, y_, variables_
+
+def build_model(input_dim, input2_dim, nn_type):
     if nn_type == "CNN":
-        return build_model_CNN(input_dim)
+        return build_model_CNN(input_dim, input2_dim)
     elif nn_type == "LSTM":
         return build_model_LSTM(input_dim)
     else:
@@ -90,32 +118,51 @@ def build_model_LSTM(input_dim):
     model.summary()
     return model
 
-def build_model_CNN(input_dim):
+def build_model_CNN(input1, input2):
     start = time.time()
 
-    model = Sequential()
-    model.add(Convolution1D(nb_filter=64, filter_length=2, input_shape=input_dim))
-    model.add(Activation('relu'))
-    model.add(Flatten())
-    #model.add(Dropout(0.2))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    #model = Sequential()
+    #model.add(Conv1D(filters=64, kernel_size=2, input_shape=input_dim))
+    #model.add(Activation('relu'))
+    #model.add(Flatten())
+    #model.add(Dense(64, activation='relu'))
+    #model.add(Dense(1, activation='sigmoid'))
+    #('Accuracy:', '0.7715216379711494')
+    #('F1:', '0.8041874376869391')
 
+    #('Accuracy:', '0.7626803164262448')
+    #('F1:', '0.7999607766228672')
+
+    inputs = Input(shape=input1)
+    x = Conv1D(64, 2, activation='relu')(inputs)
+    x = Flatten()(x)
+
+    variables = Input(shape=input2, name="mesa_variables")
+    y = Flatten()(variables)
+    x = concatenate([x, y])
+
+    x = Dense(64, activation='relu')(x)
+    predictions = Dense(1, activation='sigmoid')(x)
+
+    model = Model(inputs=[inputs,variables], outputs=predictions)
     model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
     print("> Compilation Time : ", time.time() - start)
 
-    model.summary()
+    #model.summary()
     return model
 
 # Process input:
 if input_type == "raw":
     print("Generating dataset from Raw signal")
-    x_train, y_train = get_data(dftrain, SEQ_LEN)
-    x_test, y_test = get_data(dftest, SEQ_LEN)
+    x_train, y_train, variables_train = get_data(dftrain, SEQ_LEN)
+    x_test, y_test, variables_test = get_data(dftest, SEQ_LEN)
 
     x_train = np.reshape(x_train, x_train.shape + (1,))
     x_test = np.reshape(x_test, x_test.shape + (1,))
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.20, random_state=42, shuffle=False)
+    variables_train = np.reshape(variables_train, variables_train.shape + (1,))
+    variables_test = np.reshape(variables_test, variables_test.shape + (1,))
+
+    x_train, x_val, y_train, y_val, variables_train, variables_val = train_test_split(x_train, y_train, variables_train, test_size=0.20, random_state=43, shuffle=False)
 
 elif input_type == "feat":
     print("Generating dataset from feature set")
@@ -130,7 +177,7 @@ elif input_type == "feat":
     x_train_feat = np.reshape(x_train_feat, x_train_feat.shape + (1,))
     x_test = np.reshape(x_test, x_test.shape + (1,))
 
-    x_train, x_val, y_train, y_val = train_test_split(x_train_feat, y_train, test_size=0.20, random_state=42, shuffle=False)
+    x_train, x_val, y_train, y_val = train_test_split(x_train_feat, y_train, test_size=0.20, random_state=43, shuffle=False)
 print("Done")
 
 
@@ -139,10 +186,14 @@ def run_experiment(input_type, epochs, batch_size, x_train, x_val, x_test, y_tra
     print("Training Shape: " ,x_train.shape[1:])
 
     if input_type == "raw":
-        model = build_model(x_train.shape[1:], NN_TYPE)
-        model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1, validation_data=(x_val, y_val))
-        print("Accuracy:", eval_acc(np.round(model.predict(x_test)), y_test))
-        print("F1:", eval_f1(np.round(model.predict(x_test)), y_test))
+        model = build_model(x_train.shape[1:], variables_train.shape[1:], NN_TYPE)
+        model.fit([x_train, variables_train], y_train, epochs=epochs, batch_size=batch_size, verbose=1, validation_data=([x_val, variables_val], y_val))
+        print("Accuracy:", eval_acc(np.round(model.predict([x_test, variables_test])), y_test))
+        print("F1:", eval_f1(np.round(model.predict([x_test, variables_test])), y_test))
+
+        #model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1, validation_data=(x_val, y_val))
+        #print("Accuracy:", eval_acc(np.round(model.predict(x_test)), y_test))
+        #print("F1:", eval_f1(np.round(model.predict(x_test)), y_test))
 
     elif input_type == "feat":
         model = build_model(x_train.shape[1:], NN_TYPE)
@@ -165,7 +216,7 @@ else:
     model = load_model("models/" + MODEL_OUTFILE)
     print("Model loaded from disk!")
 
-predictions = model.predict(x_test)
+predictions = model.predict([x_test, variables_test])
 
 dftest["p_%s_%d" % (NN_TYPE, SEQ_LEN)] = predictions
 dftest["%s_%d" % (NN_TYPE, SEQ_LEN)] = np.round(predictions)
